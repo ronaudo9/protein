7a19f7c06221cb18948a981d2a2070cd4341fa80
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2022-11-22 19:52:25 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,45 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/items',
+        destination: 'http://localhost:8000/items',
+      },
+      {
+        source: '/api/items/:slug',
+        destination: 'http://localhost:8000/items/:slug',
+      },
 
-module.exports = nextConfig
+      {
+        source: '/api/users',
+        destination: 'http://localhost:8000/users',
+      },
+      {
+        source: '/api/users/:slug',
+        destination: 'http://localhost:8000/users/:slug',
+      },
+
+      {
+        source: '/api/purchaseHistories',
+        destination: 'http://localhost:8000/purchaseHistories',
+      },
+      {
+        source: '/api/purchaseHistories/:slug',
+        destination: 'http://localhost:8000/purchaseHistories/:slug',
+      },
+
+      {
+        source: '/api/favorites',
+        destination: 'http://localhost:8000/favorites',
+      },
+      {
+        source: '/api/favorites/:slug',
+        destination: 'http://localhost:8000/favorites/:slug',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
