c6993df42b81260f809d607b8c86c48bbc907e62
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2022-12-08 16:19:31 +0900

    users/index部分の編集

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function UserLogin(cookieData: any) {
 
   const handler = (event: any) => {
     event.preventDefault();
-    fetch('/api/login', {
+    fetch(`${process.env.NEXT_PUBLIC_DATA}/api/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
