3561a9fe48cfef8d7c255245830eaf2b1c9d7383
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2022-12-20 15:52:54 +0900

    cssを追加しました

--- components/itemDisplayNew.tsx
+++ components/itemDisplayNew.tsx
@@ -14,42 +14,41 @@ export default function ItemDisplayNew({ data, searchQuery }: any) {
   return (
     <>
       <div className={styles.side}>
+        {data.map((item: any) => {
+          const MAX_LENGTH = 25;
+          let modStr = '';
+          if (item.description.length > MAX_LENGTH) {
+            modStr = item.description.substr(0, MAX_LENGTH) + '...';
+          }
 
-          {data.map((item: any) => {
-            const MAX_LENGTH = 25;
-            let modStr = '';
-            if (item.description.length > MAX_LENGTH) {
-              modStr = item.description.substr(0, MAX_LENGTH) + '...';
-            }
-
-            return (
-              <div key={item.id} className={styles.items_list1}>
-                <div className={styles.itemsImg}>
-                  <Image
-                    priority
-                    src={item.imageUrl}
-                    alt="商品画像"
-                    width={260}
-                    height={260}
-                    className={styles.imgPro}
-                  />
-                </div>
-
-                <div className={styles.ul}>
-                  <Link
-                    href={`./items/${encodeURIComponent(item.id)}`}
-                    className={styles.a}
-                  >
-                    <p className={styles.itemA}>{item.name}</p>
-                  </Link>
-                  <p className={styles.itemB}>
-                    価格 &nbsp;¥{item.price.toLocaleString()}
-                  </p>
-                  <p className={styles.item}>{modStr}</p>
-                </div>
+          return (
+            <div key={item.id} className={styles.items_list1}>
+              <div className={styles.itemsImg}>
+                <Image
+                  priority
+                  src={item.imageUrl}
+                  alt="商品画像"
+                  width={260}
+                  height={260}
+                  className={styles.imgPro}
+                />
               </div>
-            );
-          })}
+
+              <div className={styles.ul}>
+                <Link
+                  href={`./items/${encodeURIComponent(item.id)}`}
+                  className={styles.a}
+                >
+                  <p className={styles.itemA}>{item.name}</p>
+                </Link>
+                <p className={styles.itemB}>
+                  価格 &nbsp;¥{item.price.toLocaleString()}
+                </p>
+                <p className={styles.item}>{modStr}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -187,6 +187,7 @@ const ItemDetail: NextPage<{ detail: Item }> = ({ detail }) => {
     }
   };
 
+  // お気に入り登録（db.jsonへ現在の商品情報登録）
   let favs = {
     userId: Number(userId),
     itemId: [detail.id],
@@ -194,7 +195,6 @@ const ItemDetail: NextPage<{ detail: Item }> = ({ detail }) => {
   };
 
   console.log(favs);
-  // お気に入り登録（db.jsonへ現在の商品情報登録）
   const addFavoritesHandler = () => {
     fetch(`${process.env.NEXT_PUBLIC_PROTEIN_DATA}/favorites`, {
       method: 'POST',
@@ -203,6 +203,12 @@ const ItemDetail: NextPage<{ detail: Item }> = ({ detail }) => {
       },
       body: JSON.stringify(favs),
     });
+    // .then(() => {
+    //   {
+    //     router.push('/users/favorite');
+    //   }
+    // }
+    // );
   };
   // pagesのしたにお気に入りapi作成
 
