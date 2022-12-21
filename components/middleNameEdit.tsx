b540ee319b106c45a79d645a76d2d72a105f6edf
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2022-12-21 15:23:15 +0900

    エラー in feature/repair15

--- components/middleNameEdit.tsx
+++ components/middleNameEdit.tsx
@@ -3,7 +3,7 @@ import styles from '/styles/users.edit.module.css';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import{ Users2,User3 } from '../types/type';
+import{ Users2,Users3 } from '../types/type';
 
 
 export const getServerSideProps: GetServerSideProps = async ({
@@ -26,7 +26,7 @@ const MiddleNameEdit = ({
   readOnly,
 }: {
   formValues:Users2;
-  setFormValues:User3;
+  setFormValues:Users3;
   readOnly:boolean;
 }) => {
   const router = useRouter();
