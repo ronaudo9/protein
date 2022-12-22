2d623ecb1d569289951cce0952217b61e7728339
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2022-12-22 16:37:26 +0900

    8000を変更

--- pages/api/purchaseHistories.ts
+++ pages/api/purchaseHistories.ts
@@ -1,28 +1,31 @@
-import { IncomingMessage } from "http";
+import { IncomingMessage } from 'http';
 
-
-export default async function Handler(req:IncomingMessage & {
-  cookies: Partial<{
+export default async function Handler(
+  req: IncomingMessage & {
+    cookies: Partial<{
       [key: string]: string;
-  }>;
-},res:any) {
+    }>;
+  },
+  res: any
+) {
   const cookies = req.cookies;
   const r = await fetch(
-    `http://localhost:8000/carts?userId=${cookies.id}`
+    `${process.env.NEXT_PUBLIC_PROTEIN_DATA}/carts?userId=${cookies.id}`
   );
   const carts = await r.json();
 
   const purchaseHistories = {
-    userId : cookies.id,
-    items:carts
-  }
+    userId: cookies.id,
+    items: carts,
+  };
   await fetch(
-    `${process.env.NEXT_PUBLIC_PROTEIN_DATA}/purchaseHistories`,{
+    `${process.env.NEXT_PUBLIC_PROTEIN_DATA}/purchaseHistories`,
+    {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify( purchaseHistories),
+      body: JSON.stringify(purchaseHistories),
     }
   ).then(() => res.redirect('/purchase/purchased'));
-};
+}
 
 //stripe cliをインストールをインストール
