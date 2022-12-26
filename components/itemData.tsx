a287ce0ab10a1fe35a65f9f71fe108e3162bf3f1
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2022-12-26 09:37:32 +0900

    環境変数修正前に戻る in feature/repair14

--- components/itemData.tsx
+++ components/itemData.tsx
@@ -51,7 +51,7 @@ const ItemData: React.FunctionComponent<{
     event.preventDefault();
     const data = {};
     carts.forEach((cart: Item) => {
-      fetch(`http://localhost:8000/carts/${cart.id}`, {
+      fetch(`${process.env.NEXT_PUBLIC_PROTEIN_DATA}/carts/${cart.id}`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
@@ -189,7 +189,7 @@ const ItemData: React.FunctionComponent<{
               クレジット決済
             </button>
           </form>
-          <button className={styles.btnB} onClick={() => handler}>
+          <button className={styles.btnB} onClick={handler}>
             <span>代引き決済</span>
           </button>
         </section>
